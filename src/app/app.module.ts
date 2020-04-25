e152c6f6a9216994e0527f32e616add88d3e9e07
kamilbaranowski <kamil.baranowski93@gmail.com>
2020-04-25 21:52:30 +0200

    components update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { ChatroomComponent } from './chatroom/chatroom.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegistrationComponent,
+    ChatroomComponent
   ],
   imports: [
     BrowserModule,
