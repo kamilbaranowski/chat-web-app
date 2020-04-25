aa4488c8a06ced872e04be8938c941a59fbdac13
kamilbaranowski <kamil.baranowski93@gmail.com>
2020-04-25 22:13:22 +0200

    firebase configuration

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBLBVU813LJcT_38pezcEG_qOMy0D50rcY",
+    authDomain: "chatapp-19116.firebaseapp.com",
+    databaseURL: "https://chatapp-19116.firebaseio.com",
+    projectId: "chatapp-19116",
+    storageBucket: "chatapp-19116.appspot.com",
+    messagingSenderId: "1082287601628",
+    appId: "1:1082287601628:web:359331f03480ac9413c119",
+    measurementId: "G-85RC459X2P"
+  }
 };
 
 /*
