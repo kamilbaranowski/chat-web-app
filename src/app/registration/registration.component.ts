af0e9e382d57f16e78c4a4dd9cb2d105b39c1857
kamilbaranowski <kamil.baranowski93@gmail.com>
2020-04-26 21:37:36 +0200

    user registration

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-registration',
@@ -7,9 +9,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistrationComponent implements OnInit {
 
-  constructor() { }
+  username: string;
+  email: string;
+  password: string;
+  retypedPassword: string;
+
+  constructor(private authService: AuthService,
+              private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  signUp(){
+    this.authService.signUp(this.email, this.password)
+      .then(userCredential => {
+        console.log('RegistrationComponent User:');
+        console.log(userCredential);
+        const userStatus = 'online';
+        this.authService.setUserData(userCredential.user.uid, 
+                                    this.username, userCredential.user.email, userStatus);
+        this.router.navigate(['chat']);
+      })
+      .catch(error => {
+        console.log('Sign Up error: ' + error);
+      })
+
   }
 
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,9 +1,29 @@
 import { Injectable } from '@angular/core';
-
+import { AngularFireAuth } from '@angular/fire/auth';
+import { AngularFireDatabase } from '@angular/fire/database';
+import { Observable } from "rxjs";
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+
+  constructor(private angularFireAuth: AngularFireAuth,
+              private angularFireDatabase: AngularFireDatabase) { 
+               }
+
+  signUp(email: string, password: string){
+    return this.angularFireAuth.createUserWithEmailAndPassword(email, password);
+  }
+
+  setUserData(userId: string, username: string, email: string, status: string){
+    const path = `users/${userId}`;
+    const userData = {
+      email: email,
+      username: username,
+      status: status
+    };
+    this.angularFireDatabase.object(path).update(userData);
+  }
+
 }
