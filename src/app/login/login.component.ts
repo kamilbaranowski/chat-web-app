87892f5fe967fb3e309c21a27a3bb6d335cbc271
kamilbaranowski <kamil.baranowski93@gmail.com>
2020-06-17 14:25:39 +0200

    login component main logic

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -16,16 +16,30 @@ export class LoginComponent implements OnInit {
               private router: Router) { }
 
   ngOnInit(): void {
+    if (localStorage.getItem("token") != null) {
+      this.router.navigate(['chat']);
+    }
   }
 
   signIn(){
-    this.authService.signIn(this.email, this.password)
+    this.authService.signIn(this.email, this.password).subscribe(
+      result => {
+      console.log(result['token'])
+      localStorage.setItem("token", result['token']);
+      this.authService.signInFirebase(result['token'])
       .then(loginStatus => {
-        console.log('Sign In Success:');
-        console.log(loginStatus);
-        this.router.navigate(['chat']);
+        console.log('Sign In Success: ' + JSON.stringify(loginStatus));
+        this.authService.getIdToken().subscribe(idToken => {
+          localStorage.setItem("idToken", JSON.stringify(idToken));
+          this.router.navigate(['chat']);
+        })
+
       })
       .catch(error => console.log(error));
+      },
+      error => {
+        console.log("Error while login: " + JSON.stringify(error));
+      })
   }
 
 }
