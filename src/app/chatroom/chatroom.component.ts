77ddbfbee4c4a56a818ffd56ce32a8e88b60e25e
kamilbaranowski <kamil.baranowski93@gmail.com>
2020-06-17 14:25:08 +0200

    chatroom component main logic

--- src/app/chatroom/chatroom.component.ts
+++ src/app/chatroom/chatroom.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { ChatService } from '../services/chat.service';
+import { User } from '../model/User';
+import { Message } from '../model/Message';
+import { AuthService } from '../services/auth.service';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-chatroom',
@@ -7,9 +13,64 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChatroomComponent implements OnInit {
 
-  constructor() { }
+  currentUserUid: string;
+  message: string = '';
+  receiver: User = null;
+  messages: Message[] = new Array;
+  constructor(private chatService: ChatService, 
+              private firebaseAuth: AuthService,
+              private router: Router) {
+
+}
 
   ngOnInit(): void {
+    if (localStorage.getItem("token") == null) {
+      this.router.navigate(['login']);
+    }
   }
 
+
+  receiveEvent(receiver: User){
+    console.log('Chatroom receive: ' + receiver);
+    this.receiver = receiver;
+    this.getMessages();
+  }
+
+  sendMessage(){
+    if (this.receiver !== null && this.message != ''){
+      console.log(this.message);
+      let currentMessage = new Message(this.currentUserUid, this.receiver.uid, this.message, Date.now());
+      this.chatService.sendMessage(currentMessage)
+        .then(result => {
+          console.log("Message sent: " + JSON.stringify(result));
+        })
+        .catch(error => {
+          console.log("Error while sending message: " + JSON.stringify(error));
+        });
+    }
+    else {
+      console.log("receiver: " + JSON.stringify(this.receiver));
+      console.log("message: " + this.message);
+    }
+    this.message = '';
+  }
+  
+  handleSubmit(){
+
+  }
+
+  getMessages(){
+    const currentUsr = localStorage.getItem("currentUser");
+    this.currentUserUid = JSON.parse(currentUsr)['uid'];
+    console.log("Current User UID: " + this.currentUserUid);
+    this.chatService.getMessages(this.currentUserUid, this.receiver.uid).snapshotChanges().subscribe(messages => {
+      this.messages.splice(0);
+        messages.forEach(message => {
+          this.messages.push(message.payload.toJSON() as Message);
+        }) 
+    },
+    error => {
+      console.log(error);
+    })
+  }
 }
