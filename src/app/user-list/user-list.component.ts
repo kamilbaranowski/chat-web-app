3ebf2e9a082f14314cc1f5f0cb6b40ff860266ee
kamilbaranowski <kamil.baranowski93@gmail.com>
2020-06-17 14:36:30 +0200

    user list component main logic

--- src/app/user-list/user-list.component.ts
+++ src/app/user-list/user-list.component.ts
@@ -1,30 +1,42 @@
-import { Component, OnInit, OnChanges } from '@angular/core';
+import { Component, OnInit, OnChanges, Output } from '@angular/core';
 import { User } from '../model/User';
 import { ChatService } from '../services/chat.service';
+import { EventEmitter } from '@angular/core';
+
+
+
 
 @Component({
   selector: 'app-user-list',
   templateUrl: './user-list.component.html',
   styleUrls: ['./user-list.component.css']
 })
-export class UserListComponent implements OnInit, OnChanges {
+export class UserListComponent implements OnInit {
 
-  users: User[];
-
+  users: User[] = new Array;
+  @Output() notifyChatroomComponent = new EventEmitter();
   constructor(private chatService: ChatService) { }
 
   ngOnInit(): void {
-    this.chatService.getUsers().valueChanges().subscribe(usersList => {
-        this.users = usersList;
-        console.log(this.users);
-    })
+   this.chatService.getUsers().subscribe(result => {
+     console.log(result);
+     Object.keys(result).forEach(key => {
+          console.log(result[key]['email']);
+          this.users.push(result[key] as User);
+       });
+      
+     console.log(this.users);
+     
+   },
+   error => {
+    console.log("Error while fetching users: " + JSON.stringify(error));
+   })
+   console.log(this.users);
   }
 
-  ngOnChanges(){
-    this.chatService.getUsers().valueChanges().subscribe(usersList => {
-      this.users = usersList;
-      console.log(this.users);
-  })
+  showThread(user: User){
+    console.log("Showing thread... " + JSON.stringify(user));
+    this.notifyChatroomComponent.emit(user);
   }
 
 }
