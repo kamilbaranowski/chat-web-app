830ff410dc4a0b9cb251683a3d51c0e6447c6159
kamilbaranowski <kamil.baranowski93@gmail.com>
2020-06-17 14:34:49 +0200

    services main logic

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,24 +1,38 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { AngularFireDatabase } from '@angular/fire/database';
-import { Observable } from "rxjs";
+import { Observable , of } from "rxjs";
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { User } from '../model/User';
+import { ConnectionInfo } from '../model/ConnectionInfo';
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-
-  constructor(private angularFireAuth: AngularFireAuth,
+  connectionInfo: ConnectionInfo;
+  constructor(private httpClient: HttpClient,
+              private angularFireAuth: AngularFireAuth,
               private angularFireDatabase: AngularFireDatabase) { 
+                this.connectionInfo = new ConnectionInfo();
                }
 
-  signUp(email: string, password: string){
-    return this.angularFireAuth.createUserWithEmailAndPassword(email, password);
+  signUp(user){
+    let body = new URLSearchParams()
+    body.set("username", user.username)
+    body.set("email", user.email)
+    body.set("password", user.password)
+    body.set("status", "online")
+    let options = {
+      headers: new HttpHeaders().set('Content-Type', 'application/x-www-form-urlencoded')
+    };
+    return this.httpClient.post(this.connectionInfo.getServer() + "/register", body.toString(), options);
   }
 
   setUserData(userId: string, username: string, email: string, status: string){
     const path = `users/${userId}`;
     const userData = {
+      uid: userId,
       email: email,
       username: username,
       status: status
@@ -26,8 +40,52 @@ export class AuthService {
     this.angularFireDatabase.object(path).update(userData);
   }
 
-  signIn(email: string, password: string){
-    return this.angularFireAuth.signInWithEmailAndPassword(email, password);
+  signIn(email: string, password: string){    
+    let body = new URLSearchParams()
+    body.set("email", email)
+    body.set("password", password)
+    let options = {
+      headers: new HttpHeaders().set('Content-Type', 'application/x-www-form-urlencoded')
+    };
+    return this.httpClient.post(this.connectionInfo.getServer() + "/", body.toString(), options);
   }
 
+  signInFirebase(token: string){
+    return this.angularFireAuth.signInWithCustomToken(token)
+  }
+
+  listenForAuthStateChanges(){
+    this.angularFireAuth.onAuthStateChanged(user => {
+      if (user) {
+        //console.log("User logged in: " + JSON.stringify(user));
+        localStorage.setItem("currentUser", JSON.stringify(user));
+      } else {
+        console.log("User logged out.")
+      }
+    });
+    
+  }
+
+  getUsernameByUid(uid: string){
+    return this.httpClient.get(this.connectionInfo.getServer() + `/users?uid=${uid}`);
+  }
+
+  logout() {
+    localStorage.clear();
+    return this.angularFireAuth.signOut;
+  }
+
+  getIdToken() {
+    const idToken = this.angularFireAuth.idToken;
+    console.log("ID TOKEN: ");
+    console.log(idToken);
+    return idToken;
+  }
+  verifyLoggedInUser() {
+    if (localStorage.getItem('token') != null) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 }
--- src/app/services/chat.service.ts
+++ src/app/services/chat.service.ts
@@ -1,25 +1,47 @@
 import { Injectable } from '@angular/core';
-import { AngularFireDatabase, AngularFireList } from '@angular/fire/database';
+import { AngularFireDatabase, AngularFireList, AngularFireObject } from '@angular/fire/database';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { User } from '../model/User';
-
+import { Message } from '../model/Message';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { ConnectionInfo } from '../model/ConnectionInfo';
 @Injectable({
   providedIn: 'root'
 })
 export class ChatService {
-
+  connectionInfo: ConnectionInfo;
+  messages: AngularFireList<Message>;
   constructor(private angularFireDatabase: AngularFireDatabase,
-    private angularFireAuth: AngularFireAuth) {
-
+    private angularFireAuth: AngularFireAuth,
+    private httpClient: HttpClient) {
+      this.connectionInfo = new ConnectionInfo();
   }
 
-  getUsers(): AngularFireList<User>{
-    return this.angularFireDatabase.list('/users', user => {
-      return user.orderByKey();
-    })
+  getUsers() {
+    let options = {
+      headers: new HttpHeaders().set('Authorization', 'Bearer ' + localStorage.getItem('idToken'))
+  };
 
-    
+    return this.httpClient.get(this.connectionInfo.getServer() + "/users", options);
   }
 
+  getUserById(userId: string): AngularFireObject<User>{
+    const path = `users/${userId}`;
+    return this.angularFireDatabase.object(path);
+  }
+
+  sendMessage(message: Message){
+    this.angularFireDatabase.database
+      .ref(`/messages/${message.receiver}/${message.sender}`)
+      .push(message);
+    return this.angularFireDatabase.database
+      .ref(`/messages/${message.sender}/${message.receiver}`)
+      .push(message);
+  }
+
+  getMessages(sender: string, receiver: string): AngularFireList<Message>{
+    return this.angularFireDatabase.list(`/messages/${sender}/${receiver}`, ref => {
+      return ref.limitToLast(25).orderByKey();
+    });
+  }
 }
-
